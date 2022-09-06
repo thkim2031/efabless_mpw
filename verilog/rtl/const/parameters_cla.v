2e0e7588cff396e82bed2730976e2966318a0c8a
thkim <thkim4663@naver.com>
2022-09-06 19:02:15 +0900

    1

---
+++ verilog/rtl/const/parameters_cla.v
@@ -0,0 +1,14 @@
+
+  //######################//
+ // 	CLA Parameters      //
+//######################//
+//for test bench
+`define P_W 16
+parameter N=16;
+
+//for main module
+parameter N_16=16;
+parameter WIDTH=32;
+
+
+
---
+++ verilog/rtl/const/parameters_r8mb8.v
@@ -0,0 +1,17 @@
+
+  //######################//
+ // 	CLAs Parameters      //
+//######################//
+//CLA_9
+`define P_W_9 9
+parameter N_9=9;
+
+//CLA_16
+`define P_W_1 16
+parameter N=16;
+  //#######################//
+ // Multiplier Parameters //
+//#######################//
+
+parameter WIDTH = 8;
+parameter group_cnt=(WIDTH>>2)+1; 
