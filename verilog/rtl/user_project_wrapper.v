0921ca5e15a22e16cb4dba62b2e32241e63afaae
thkim <thkim4663@naver.com>
2022-09-08 23:02:24 +0900

    modify gds lef verilog

--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -72,7 +72,7 @@ module user_project_wrapper #(
     inout [`MPRJ_IO_PADS-10:0] analog_io,
 
     // Independent clock (on independent integer divider)
-    input   user_clock2,
+    input   clk_50,
 
     // User maskable interrupt signals
     output [2:0] user_irq
@@ -82,42 +82,154 @@ module user_project_wrapper #(
 /* User project is instantiated  here   */
 /*--------------------------------------*/
 
-user_proj_example mprj (
-`ifdef USE_POWER_PINS
-	.vccd1(vccd1),	// User area 1 1.8V power
-	.vssd1(vssd1),	// User area 1 digital ground
-`endif
 
-    .wb_clk_i(wb_clk_i),
-    .wb_rst_i(wb_rst_i),
 
-    // MGMT SoC Wishbone Slave
 
-    .wbs_cyc_i(wbs_cyc_i),
-    .wbs_stb_i(wbs_stb_i),
-    .wbs_we_i(wbs_we_i),
-    .wbs_sel_i(wbs_sel_i),
-    .wbs_adr_i(wbs_adr_i),
-    .wbs_dat_i(wbs_dat_i),
-    .wbs_ack_o(wbs_ack_o),
-    .wbs_dat_o(wbs_dat_o),
 
-    // Logic Analyzer
+    FFPMAC ffpmac_0(
+	
+//		`ifdef USE_POWER_PINS
+//		.vccd1(vccd1),	// User area 1 1.8V power
+//		.vssd1(vssd1),	// User area 1 digital ground
+//		`endif
+	    .A(la_data_in[15:0]),
+	    .B(la_data_in[31:16]), 
+	    .C(la_data_in[63:32]),
+	    .rnd(la_oenb[1:0]),
+	    .clk(clk_50),
+	    .rst(wb_rst_i),
+	    .result(la_data_out[31:0]));
 
-    .la_data_in(la_data_in),
-    .la_data_out(la_data_out),
-    .la_oenb (la_oenb),
+    CLA_16 cla16_0( 
+//		`ifdef USE_POWER_PINS
+//		.vccd1(vccd1),	// User area 1 1.8V power
+//		.vssd1(vssd1),	// User area 1 digital ground
+//		`endif
+	    .OPA(la_data_in[79:64]),
+	    .OPB(la_data_in[95:80]),
+	    .CIN(la_oenb[2]), .PHI(la_oenb[3]),
+	    .SUM_FINAL(la_data_out[47:32]),
+	    .COUT_FINAL(la_data_out[48]),
+	    .CLK(wb_clk_i));
 
-    // IO Pads
+    sa_2D sa2d_0( 
+	
+//		`ifdef USE_POWER_PINS
+//		.vccd1(vccd1),	// User area 1 1.8V power
+//		.vssd1(vssd1),	// User area 1 digital ground
+//		`endif
+	    .AA(la_data_in[103:96]), 
+	    .BB(la_data_in[111:104]), 
+	    .CLK(wb_clk_i), 
+	    .SHIFTEN(la_oenb[5:4]),
+	    .RST(wb_rst_i), 
+	    .Y(la_data_out[80:49]));
 
-    .io_in (io_in),
-    .io_out(io_out),
-    .io_oeb(io_oeb),
+   r8_mb8 r8_mb8_0(
+   
+//		`ifdef USE_POWER_PINS
+//		.vccd1(vccd1),	// User area 1 1.8V power
+//		.vssd1(vssd1),	// User area 1 digital ground
+//		`endif
+	    .mx(la_data_in[119:112]),
+	    .my(la_data_in[127:120]),
+	    .CLK(wb_clk_i), 
+	    .RST(wb_rst_i),
+	    .product_final(la_data_out[106:81]));
 
-    // IRQ
-    .irq(user_irq)
-);
+
+
+
 
 endmodule	// user_project_wrapper
+
+
+/*
+
+(* blackbox *)
+
+module FFPMAC(A, B, C, rnd, clk, rst,  result);
+//Parameters
+`ifdef USE_POWER_PINS
+    inout vccd1,        // User area 1 1.8V supply
+    inout vssd1,        // User area 1 digital ground
+`endif
+  parameter WIDTH_16 = 16;
+  parameter WIDTH_32 = 32;
+ 
+  //I/O decalarations
+  input [WIDTH_16-1:0] A,B;
+  input [WIDTH_32-1:0] C;
+  input [1:0] rnd;
+  input clk, rst;
+  output [WIDTH_32-1:0] result;
+ 
+  endmodule
+
+module CLA_16 ( OPA, OPB, CIN, PHI, SUM_FINAL, COUT_FINAL, CLK );
+
+  parameter N_16 = 16;
+  `ifdef USE_POWER_PINS
+    inout vccd1,        // User area 1 1.8V supply
+    inout vssd1,        // User area 1 digital ground
+  `endif
+  input  [N_16-1:0] OPA;
+  input  [N_16-1:0] OPB;
+  input  CIN;
+  input  PHI;
+  input  CLK;
+  output [N_16-1:0] SUM_FINAL;
+  output COUT_FINAL;
+
+  endmodule
+
+  
+module sa_2D( AA, BB, CLK,SHIFTEN ,RST, Y);
+
+  parameter HPE=2;  // step 1 horizontal processing elements
+  parameter VPE=2;  // vertical processing elements
+
+  parameter WIDTH_4=4;   // step 2 operands width  
+  `ifdef USE_POWER_PINS
+    inout vccd1,        // User area 1 1.8V supply
+    inout vssd1,        // User area 1 digital ground
+  `endif
+  input   [WIDTH_4*HPE-1:0]  AA;
+  input   [WIDTH_4*HPE-1:0]  BB;
+  input           CLK;
+  input           RST;
+  input   [1:0]      SHIFTEN;
+  output   [(2*WIDTH_4*HPE*VPE)-1:0]  Y;
+
+  endmodule
+
+
+module r8_mb8(mx,my,CLK,RST,product_final);
+  parameter WIDTH_8 = 8;
+  `ifdef USE_POWER_PINS
+    inout vccd1,        // User area 1 1.8V supply
+    inout vssd1,        // User area 1 digital ground
+  `endif
+  //IO Start
+  input wire [WIDTH_8-1:0] mx;
+  input wire [WIDTH_8-1:0] my;
+  input wire CLK;
+  input wire RST;
+  output reg [(WIDTH_8*2)-1:0] product_final;
+
+  endmodule
+
+
+module clock_divider(clk,clock_out);
+	`ifdef USE_POWER_PINS
+//    inout vccd1,        // User area 1 1.8V supply
+//    inout vssd1,        // User area 1 digital ground
+	`endif
+    //IO Start
+	input clk;
+	input clock_out;
+endmodule
+
+*/
 
 `default_nettype wire
